4dc33f44732c8e466452eea5db4b6efbfebe6871
David Martinez de Silva <dmtzds@gmail.com>
2020-10-12 22:04:15 -0500

    lecture 139

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,6 +26,7 @@ const buildControls = (props) => (
     <button
       disabled={!props.purchasable}
       className="OrderButton"
+      onClick={props.ordered}
     >
       ORDER NOW
     </button>
